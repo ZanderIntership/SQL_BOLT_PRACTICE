b46d5af762bdc7d81708f3d87a6e88aab13f3160
ZanderIntership <ZanderWepener5@gmail.com>
2025-11-14 13:34:19 +0200

    Update Dump_Info.sql

--- Dump_Info.sql
+++ Dump_Info.sql
@@ -19,3 +19,29 @@ FROM buildings
   LEFT JOIN employees
     ON building_name = building
 WHERE role IS NULL;
+
+
+SELECT 
+    movies.title,
+    (COALESCE(boxoffice.domestic_sales, 0) 
+     + COALESCE(boxoffice.international_sales, 0)) / 1000000 
+        AS combined_sales_millions
+FROM movies
+LEFT JOIN boxoffice
+    ON movies.id = boxoffice.movie_id;
+
+
+SELECT 
+    movies.title, ROUND((boxoffice.rating / 10) * 100,2) AS Percent_Rating
+FROM movies
+LEFT JOIN boxoffice
+    ON movies.id = boxoffice.movie_id;
+
+SELECT 
+    movies.title
+FROM movies
+        WHERE movies.year % 2 = 0
+
+
+SELECT *, AVG(employees.years_employed) FROM employees
+    GROUP BY employees.role;
